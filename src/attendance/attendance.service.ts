163817ff5cd892e0db7234bb669c81d0914f3dcc
nomi035 <noamantariq@gmail.com>
2025-12-31 10:58:03 +0500

    attendance updated

--- src/attendance/attendance.service.ts
+++ src/attendance/attendance.service.ts
@@ -97,8 +97,7 @@ export class AttendanceService {
   endDate: Date,
   id:number
 ) {
-  startDate.setHours(0, 0, 0, 0);
-  endDate.setHours(23, 59, 59, 999);
+ 
   const attendances = await this.attendanceRepository
     .createQueryBuilder('attendance')
     .leftJoinAndSelect('attendance.employee', 'employee')
